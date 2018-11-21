1200210aa2e3b10b0e0d9e1a85ecd56062f423bd
Amy <amy_ludwig@live.com>
2018-11-21 18:01:09 -0500

    added working filter to selection

--- jeopardy.js
+++ jeopardy.js
@@ -5,26 +5,65 @@
      //App Init
      //Document Ready
 
+//-------------------------MASTER APP OBJECT AND STEADY VARIABLES--------------------
+const app = {};
+app.url = "http://jservice.io/api/";
+
 /*----------------------------------------------*/
 
+console.log("hey there")
+
+//-------------------------API CALL FUNCTION--------------------
+
+app.call = function (categoryID) {
+     $.ajax({
+          url: `${app.url}clues`,
+          method: 'GET',
+          data: {
+               category: categoryID,
+               // value: 400,
+               // count: 4,
+          }
+     }).then((res) => {
+          app.questionList = res;
+          console.log(app.questionList);
+          // app.questionList.map((item) => {
+          //      console.log(`CATEGORY: ${item.category.title}`)
+          //      console.log(`HERE IS THE QUESTION: ${item.question}
+          //                ANSWER: ${item.answer}  ${item.value}
+          //                `)
+          // })
+     });
+}
+
+
+app.click = function(){
+
+     $('.category').on('click', function() {
+          app.categoryChoice = $(this)[0].id;
+          console.log(app.categoryChoice);
+          // console.log($(this));
+          app.call(app.categoryChoice)
+     })
+
+}
 
 
 
 
 
-//-------------------------MASTER APP OBJECT--------------------
-const app = {};
 
 
 //-------------------------APP INIT---------------------------
 
 app.init = function(){
-
+     // app.call();
+     app.click();
 };
 
 //-------------------------DOCUMENT READY----------------------
 
 $(function () {
-
+     app.init();
 
 });
