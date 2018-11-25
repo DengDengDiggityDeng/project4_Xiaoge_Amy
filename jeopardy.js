75dcc7bd383901bbcfb0b94c752ebeb1e8f3bc56
Xiaoge Deng <xiaoge.deng@hotmail.com>
2018-11-25 15:33:36 -0500

    Add files via upload

--- jeopardy.js
+++ jeopardy.js
@@ -1,9 +1,8 @@
-
 //CONTENT: 
 
-     //Master object
-     //App Init
-     //Document Ready
+//Master object
+//App Init
+//Document Ready
 
 //-------------------------MASTER APP OBJECT AND STEADY VARIABLES--------------------
 const app = {};
@@ -18,33 +17,33 @@ console.log("hey there")
 //CALL QUESTIONS:
 
 app.call = function (categoryID, changed) {
-     $.ajax({
-          url: `${app.url}clues`,
-          method: 'GET',
-          data: {
-               category: categoryID,
-               offset: changed,
-          }
-     }).then((res) => {
-          app.filteredRes = res.filter(question => question.question !== "");
-          app.displayResults(app.filteredRes);          
-     });
+    $.ajax({
+        url: `${app.url}clues`,
+        method: 'GET',
+        data: {
+            category: categoryID,
+            offset: changed,
+        }
+    }).then((res) => {
+        app.filteredRes = res.filter(question => question.question !== "");
+        app.displayResults(app.filteredRes);
+    });
 }
 
 //CALL CATEGORIES:
 
 app.callCategories = function (offset) {
-     $.ajax({
-          url: `${app.url}categories`,
-          method: 'GET',
-          data: {
-               count: 3,
-               offset: offset,
-          }
-     }).then((res) => {
-          console.log(res);
-          app.displayCategories(res);          
-     });
+    $.ajax({
+        url: `${app.url}categories`,
+        method: 'GET',
+        data: {
+            count: 3,
+            offset: offset,
+        }
+    }).then((res) => {
+        console.log(res);
+        app.displayCategories(res);
+    });
 }
 
 
@@ -53,93 +52,93 @@ app.callCategories = function (offset) {
 //Print question results
 
 app.displayResults = function (questions) {
-     app.fiveQuestions = questions.slice(0,1);
-     
-          app.fiveQuestions.forEach((q) => {
-               if (q.question !== "") {
-               $('.clue').html(
-               `    <div class="question">
+    app.fiveQuestions = questions.slice(0, 1);
+
+    app.fiveQuestions.forEach((q) => {
+        if (q.question !== "") {
+            $('.clue').html(
+                `    <div class="question">
                          <p class="points">$${q.value}</p>
                          <p>${q.question} <p> 
                     </div>`)
 
-               $('.answer').html(
-               `<div>
+            $('.answer').html(
+                `<div>
                     <p>${q.answer}</p>
-                    <p>Jeopardy Air Date: ${q.airdate.substring(0,10)}</p>
+                    <p>Jeopardy Air Date: ${q.airdate.substring(0, 10)}</p>
                </div>`)
 
-               $('.question-category').html(`${q.category.title}`);
-          }
+            $('.question-category').html(`${q.category.title}`);
+        }
 
 
-          }); 
-     }
+    });
+}
 
 //Print category results
 
-app.displayCategories = function (categories) {     
-     $('.category1').html(`<p id="${categories[0].id}">${categories[0].title} </p>`);
-     $('.category2').html(`<p id="${categories[1].id}">${categories[1].title} </p>`);
-     $('.category3').html(`<p id="${categories[2].id}">${categories[2].title} </p>`);
- }; 
+app.displayCategories = function (categories) {
+    $('.category1').html(`<p id="${categories[0].id}">${categories[0].title} </p>`);
+    $('.category2').html(`<p id="${categories[1].id}">${categories[1].title} </p>`);
+    $('.category3').html(`<p id="${categories[2].id}">${categories[2].title} </p>`);
+};
 
 //-------------------------Change offset of questions and categories --------------------
 
 //Question offset change function
 
-app.changeOffset = function() {
-     app.offsetValue = 0
-     $('.next-question').on('click', function(){
-          $('.answer').removeClass('show');
-          $('.answer').addClass('hide');
-          if (app.offsetValue < 5) {
-               app.offsetValue++; 
-          } else {
-               app.offsetValue = 0;
-          }
-          app.call(app.categoryChoice, app.offsetValue)
-          return app.offsetValue;
-     })
+app.changeOffset = function () {
+    app.offsetValue = 0
+    $('.next-question').on('click', function () {
+        $('.answer').removeClass('show');
+        $('.answer').addClass('hide');
+        if (app.offsetValue < 5) {
+            app.offsetValue++;
+        } else {
+            app.offsetValue = 0;
+        }
+        app.call(app.categoryChoice, app.offsetValue)
+        return app.offsetValue;
+    })
 }
 
 //category offset change function
 
 app.catoffsetValue = 10 // category call offset
 
-app.newCategories = function(){
-     $('.change-category').on('click', function() {
-          $('.answer').removeClass('show');
-          $('.answer').addClass('hide');
-          if (app.catoffsetValue < 1000) {
-               app.catoffsetValue = app.catoffsetValue + 3;
-          } else {
-               app.catoffsetValue = 0;
-          }
-          app.callCategories(app.catoffsetValue)
-          return app.catoffsetValue;
-     })
+app.newCategories = function () {
+    $('.change-category').on('click', function () {
+        $('.answer').removeClass('show');
+        $('.answer').addClass('hide');
+        if (app.catoffsetValue < 1000) {
+            app.catoffsetValue = app.catoffsetValue + 3;
+        } else {
+            app.catoffsetValue = 0;
+        }
+        app.callCategories(app.catoffsetValue)
+        return app.catoffsetValue;
+    })
 }
 
 
 
 //-------------------------SELECT CATEGORY FUNCTION--------------------
 
-app.click = function(){
+app.click = function () {
 
-     $('.category').on('click', function() {
+    $('.category').on('click', function () {
+        app.offsetValue = 0;
+        // console.log($(this).find('p')[0].id);          
+        app.categoryChoice = $(this).find('p')[0].id;
+        app.call(app.categoryChoice, app.offsetValue)
+        $('.pick-first').addClass('display-none');
+        $('.show-answer').removeClass('hide');
+        $('.answer').removeClass('show');
+        $('.answer').addClass('hide');
+        $('.next-question').removeClass('hide');
+        $('.category-p').removeClass('hide');
 
-          // console.log($(this).find('p')[0].id);          
-          app.categoryChoice = $(this).find('p')[0].id;          
-          app.call(app.categoryChoice, app.offsetValue)
-          $('.pick-first').addClass('display-none');
-          $('.show-answer').removeClass('hide');
-          $('.answer').removeClass('show');
-          $('.answer').addClass('hide');
-          $('.next-question').removeClass('hide');
-          $('.category-p').removeClass('hide');
-
-     })
+    })
 }
 
 
@@ -147,22 +146,33 @@ app.click = function(){
 //-------------------------Show answer---------------------------
 
 app.answerToggle = function () {
-     $('.show-answer').on('click', function () {
-          $('.answer').removeClass('hide');
-          $('.answer').addClass('show');
-     });
+    $('.show-answer').on('click', function () {
+        $('.answer').removeClass('hide');
+        $('.answer').addClass('show');
+    });
 }
+
+//------------------------- parallax effect --------------------
+
+
+document.addEventListener('scroll', () => {
+    let parent = document.getElementById('parallax-container');
+    let children = parent.getElementsByTagName('div');
+    for (let i = 0; i < children.length; i++) {
+        children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+    }
+}, false)
 
 
 
 //-------------------------APP INIT---------------------------
 
-app.init = function(){
-     app.click();
-     app.changeOffset();
-     app.callCategories(app.catoffsetValue);
-     app.answerToggle();
-     app.newCategories();
+app.init = function () {
+    app.click();
+    app.changeOffset();
+    app.callCategories(app.catoffsetValue);
+    app.answerToggle();
+    app.newCategories();
 
 
 };
@@ -170,5 +180,5 @@ app.init = function(){
 //-------------------------DOCUMENT READY----------------------
 
 $(function () {
-     app.init();
+    app.init();
 });
