bddda5a343ad7fbdccdccc19a202d7312b5070d5
Xiaoge Deng <xiaoge.deng@hotmail.com>
2018-11-25 15:43:56 -0500

    Add files via upload

--- jeopardy.js
+++ jeopardy.js
@@ -152,17 +152,6 @@ app.answerToggle = function () {
     });
 }
 
-//------------------------- parallax effect --------------------
-
-
-document.addEventListener('scroll', () => {
-    let parent = document.getElementById('parallax-container');
-    let children = parent.getElementsByTagName('div');
-    for (let i = 0; i < children.length; i++) {
-        children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
-    }
-}, false)
-
 
 
 //-------------------------APP INIT---------------------------
